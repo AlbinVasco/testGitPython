1eae80f991c7cd381c7e79f1a023f6f70a39f45f
Albin Källner <albinkallner1@gmail.com>
2021-09-28 08:20:32 +0200

    Lade till en if-sats som kollar om x är noll

--- test.py
+++ test.py
@@ -8,3 +8,9 @@
 
 print("Detta är ett test för att se ifall jag kan få upp det på git")
 print("Testar att ändra lite")
+x = 0
+if x == 0 {
+    print("x är noll")
+}else{
+    print("x är inte noll")
+}
